546c909203e4a7380730636ace6c3419aecd1d5b
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-02-13 16:51:47 +0100

    update menu i add weapon

--- game_inventory.py
+++ game_inventory.py
@@ -1,19 +1,33 @@
 
 #git firts commmit
+added_items = {}
+inventory = {'shotgun':1,'ak47':1, 'knife':1, 'pistol':1}
 
+def main():
+    print('-'*55)
+    choosen_path = input("Welcome in th game inventory, what would You like to do: \n 1 - Add to inventory \n 2 - Remove from inventory \n 3 - XXXX \n 4 - XXX \n 5 - Quit inventory \n")
+    
+def choosen_path():    
+    if input == 1:
+        add_to_inventory(inventory, added_items)
+    elif input == 2:
+        print("remove")
 
-inventory = {'shotgun':1,'ak47':1, 'knife':1,
-                'pistol':1}
 
 def display_inventory(inventory):
-    """Display the contents of the inventory in a simple way."""
-    pass
-
+    for key, value in inventory.items():
+        print("Weapon type:", key,"|","ammo :", value)
+    
+   
 
 def add_to_inventory(inventory, added_items):
-    """Add to the inventory dictionary a list of items from added_items."""
-    pass
-
+    added_items = input("Please add a weapon or enter N to main menu :" )
+    if added_items not in inventory.keys():
+        inventory.setdefault(added_items, 1 )
+    elif added_items in inventory.keys():
+        inventory[added_items] += 1
+    display_inventory(inventory)
+    add_to_inventory(inventory, added_items)
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
@@ -39,3 +53,8 @@ def export_inventory(inventory, filename):
     """Export the inventory into a CSV file."""
 
     pass
+
+if __name__ == '__main__':
+    main()
+    display_inventory(inventory)
+    add_to_inventory(inventory, added_items)
