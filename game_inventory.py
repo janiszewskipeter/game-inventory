910a1dad678d996da74d04812e12fd51f0de6b7a
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-03-16 14:51:40 +0100

    order bug fixed,move on to csv file import /export

--- game_inventory.py
+++ game_inventory.py
@@ -1,17 +1,36 @@
 import sys
+import csv
 added_items = {}
 removed_items = {}
+line = "_"*55
 order = []
 inventory = {'shotgun':2,'ak47':5, 'knife':8, 'pistol':1, 'uzi':5, 'ckm':1}
 
 
+def main():
+    print(line)
+    choose = input("Welcome in th game inventory, what would You like to do: \n 1 - Add to inventory \n 2 - Remove from inventory \n 3 - Print by order \n 4 - Import inventory \n 5 - Export inventory \n 6 - Quit inventory \n Enter a number: \n")
+    if choose == "1":
+        add_to_inventory(inventory, added_items)
+    elif choose == "2":
+        remove_from_inventory(inventory, removed_items)
+    elif choose == "3":
+        print("Inventory by order")
+        print(line)
+        print_table(inventory, order)
+    elif choose == "4":
+        print("Import inventory")
+    elif choose == "5":
+        print("Export inventory")    
+    elif choose == "6":
+        sys.exit()
+
 
 def display_inventory(inventory):
-    print("Your current inventory is: \n")
-    print("_"*55)
+    print(line)
     for k, v in inventory.items():
         print("Weapon type: " ,k ,"|","ammo : ", v)
-    print("_"*55)
+    print(line)
    
 
 def add_to_inventory(inventory, added_items):
@@ -24,6 +43,7 @@ def add_to_inventory(inventory, added_items):
         inventory[added_items] += 1
     display_inventory(inventory)
     add_to_inventory(inventory, added_items)
+
 
 def remove_from_inventory(inventory, removed_items):
     display_inventory(inventory)
@@ -40,15 +60,24 @@ def remove_from_inventory(inventory, removed_items):
 
 def print_table(inventory, order):
     order = input("What type of order would You like?\n No answer = no order \n desc - descending order \n asc -ascending order\n")
-    OrderedDict = {}
     if order == "":
         display_inventory(inventory)
-    elif order == "desc":
-        OrderedDict(sorted(inventory.items(), key=lambda t: t[0]))
-        print(OrderedDict)
+    elif order == "asc":
+        num = dict(sorted(inventory.items(), key=lambda x: x[1]))
+        print(line)
+        print("Ascending order of inventory")
+        print(line)
+        print("Item name  | count ")
+        for k, v in num.items():
+            print(k ,"      |", v)
+        main()    
     else:
-        OrderedDict(sorted(inventory.items(), key=lambda t: t[1]))
-        print(OrderedDict)
+        num = dict(sorted(inventory.items(), key=lambda x: x[1], reverse=True))
+        print("Descending order of inventory")
+        for k, v in num.items():
+            print("Weapon type: " ,k ,"|","ammo : ", v)  
+        main()
+
 
 def import_inventory(inventory, filename):
     """Import new inventory items from a CSV file."""
@@ -60,27 +89,10 @@ def export_inventory(inventory, filename):
 
     pass
 
-def main():
-    print('-'*55)
-    choose = input("Welcome in th game inventory, what would You like to do: \n 1 - Add to inventory \n 2 - Remove from inventory \n 3 - Print by order \n 4 - Import inventory \n 5 - Export inventory \n 6 - Quit inventory \n Enter a number: \n")
-    display_inventory(inventory)
-    if choose == "1":
-        add_to_inventory(inventory, added_items)
-    elif choose == "2":
-        remove_from_inventory(inventory, removed_items)
-    elif choose == "3":
-        print("Inventory by order")
-        print_table(inventory, order)
-    elif choose  == "4":
-        print("Import inventory")
-    elif choose == "5":
-        print("Export inventory")    
-    elif choose == "6":
-        sys.exit()
-    display_inventory(inventory)
+
+
+    
        
 
 if __name__ == '__main__':
     main()
-    display_inventory(inventory)
-    add_to_inventory(inventory, added_items)
